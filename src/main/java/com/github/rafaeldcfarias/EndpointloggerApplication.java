c7dfc5c20951039c2e3b58ed5a7c4abba8c331c5
rafael.farias <rafaeldcfarias@gmail.com>
2022-04-20 16:27:38 -0300

    initial

--- src/main/java/com/github/rafaeldcfarias/EndpointloggerApplication.java
+++ src/main/java/com/github/rafaeldcfarias/EndpointloggerApplication.java
@@ -1,13 +1,22 @@
 package com.github.rafaeldcfarias;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EndpointloggerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EndpointloggerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointloggerApplication.class, args);
+    }
+
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 
 }
--- src/main/java/com/github/rafaeldcfarias/controllers/LogController.java
+++ src/main/java/com/github/rafaeldcfarias/controllers/LogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@Slf4j(topic = "request")
 @RestController
 @RequestMapping("/logger")
 public class LogController {
